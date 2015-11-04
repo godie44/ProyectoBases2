473209599133e3ae910ce24230379d5a29ae8d59
godie44 <godie44@hotmail.com>
2015-11-04 00:40:34 -0600

    Creacion Clase Conexion base

--- AD/DataBaseManager.cs
+++ AD/DataBaseManager.cs
@@ -1,16 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace AD
 {
-    // This project can output the Class library as a NuGet Package.
-    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
-    public class Class1
+    public class DataBaseManager
     {
-        public Class1()
+        private static SqlConnection con;
+        
+        public SqlConnection Con
         {
+            get
+            {
+                return con;
+            }
+
+            set
+            {
+                con = value;
+            }
+        }
+
+        public static SqlConnection OpenSqlDatabase(string _user, string _pass,string _server)
+        {
+            try
+            {
+
+                if (con == null || con.State == System.Data.ConnectionState.Closed)
+                {
+                    con = new SqlConnection("Data Source=" + _server + ";User Id=" + _user + ";Password=" + _pass);
+                    con.Open();
+                }
+                return con;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
+
+        public static void CloseSqlDatabase()
+        {
+            try
+            {
+                if (con != null || con.State == System.Data.ConnectionState.Open)
+                {
+
+                    con.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
